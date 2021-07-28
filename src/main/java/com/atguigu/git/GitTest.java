51b87270f05659ecec674bc938686ab763f85ba7
cjm <522637210@qq.com>
2021-07-29 04:05:53 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author ChenJiaMing
+ * @create 2021-07-29 4:01
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
